c48e53515d9e17b56d49e6fd0a9e6f6aff601c73
Yelleti-Sunil-Sai <yelletisunilsai130@gmail.com>
2024-03-07 13:36:01 +0530

    added some changes

--- src/App.js
+++ src/App.js
@@ -227,11 +227,8 @@ function App() {
       <footer class="bg-white rounded-lg shadow m-4  ">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span class="text-sm text-gray-500 sm:text-center ">
-            © 2023{" "}
-            <a href="https://flowbite.com/" class="hover:underline">
-              Flowbite™
-            </a>
-            . All Rights Reserved.
+            © 2024 <a class="hover:underline">SunilSai™</a>. All Rights
+            Reserved.
           </span>
           <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500  sm:mt-0">
             <li>
